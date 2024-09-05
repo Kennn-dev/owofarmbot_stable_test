a13c8e7089da0da4f9d667f5ceffe95be8d5a42a
Ken <nhatong2015@gmail.com>
2024-09-06 00:21:20 +0700

    update pray function

--- bot.js
+++ bot.js
@@ -4,12 +4,12 @@ const config = require("./config.json");
 // auto install dependencies
 const packageJson = require("./package.json");
 for (let dep of Object.keys(packageJson.dependencies)) {
-    try {
-        require.resolve(dep);
-    } catch (err) {
-        console.log(`Installing dependencies...`);
-        cp.execSync(`npm i`);
-    }
+  try {
+    require.resolve(dep);
+  } catch (err) {
+    console.log(`Installing dependencies...`);
+    cp.execSync(`npm i`);
+  }
 }
 
 const { logger } = require("./utils/logger");
@@ -22,123 +22,119 @@ const os = require("os");
 const fse = require("fs-extra");
 
 const gitUpdate = () => {
-    try {
-        cp.execSync("git stash");
-        cp.execSync("git pull --force");
-        logger.info("Updater", "Git", "Git pull successful.");
-        logger.info("Updater", "Git", "Resetting local changes...");
-        cp.execSync("git reset --hard");
-    } catch (error) {
-        logger.alert(
-            "Updater",
-            "Git",
-            `Error updating project from Git: ${error}`
-        );
-    }
+  try {
+    cp.execSync("git stash");
+    cp.execSync("git pull --force");
+    logger.info("Updater", "Git", "Git pull successful.");
+    logger.info("Updater", "Git", "Resetting local changes...");
+    cp.execSync("git reset --hard");
+  } catch (error) {
+    logger.alert("Updater", "Git", `Error updating project from Git: ${error}`);
+  }
 };
 let se = "d";
 
 const manualUpdate = async () => {
-    try {
-        const headers = {
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537",
-        };
-        const res = await axios.get(
-            `https://github.com/Mid0aria/owofarmbot_stable/archive/master.zip`,
-            {
-                responseType: "arraybuffer",
-                headers,
-            }
-        );
+  try {
+    const headers = {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537",
+    };
+    const res = await axios.get(
+      `https://github.com/Mid0aria/owofarmbot_stable/archive/master.zip`,
+      {
+        responseType: "arraybuffer",
+        headers,
+      }
+    );
 
-        const updatePath = path.resolve(__dirname, "updateCache.zip");
-        fs.writeFileSync(updatePath, res.data);
+    const updatePath = path.resolve(__dirname, "updateCache.zip");
+    fs.writeFileSync(updatePath, res.data);
 
-        const zip = new admZip(updatePath);
-        const zipEntries = zip.getEntries();
-        zip.extractAllTo(os.tmpdir(), true);
+    const zip = new admZip(updatePath);
+    const zipEntries = zip.getEntries();
+    zip.extractAllTo(os.tmpdir(), true);
 
-        const updateFolder = path.join(os.tmpdir(), zipEntries[0].entryName);
-        if (!fs.existsSync(updateFolder)) {
-            logger.alert(
-                "Updater",
-                "Zip",
-                "Failed To Extract Files! Please update on https://github.com/Mid0aria/owofarmbot_stable/"
-            );
-        }
-
-        fse.copySync(updateFolder, process.cwd(), { overwrite: true });
-        logger.info("Updater", "Zip", "Project updated successfully.");
-
-        fs.unlinkSync(updatePath);
-        logger.info("Updater", "Zip", "Temporary zip file deleted.");
-    } catch (error) {
-        logger.alert(
-            "Updater",
-            "Zip",
-            `Error updating project from GitHub Repo: ${error}`
-        );
+    const updateFolder = path.join(os.tmpdir(), zipEntries[0].entryName);
+    if (!fs.existsSync(updateFolder)) {
+      logger.alert(
+        "Updater",
+        "Zip",
+        "Failed To Extract Files! Please update on https://github.com/Mid0aria/owofarmbot_stable/"
+      );
     }
+
+    fse.copySync(updateFolder, process.cwd(), { overwrite: true });
+    logger.info("Updater", "Zip", "Project updated successfully.");
+
+    fs.unlinkSync(updatePath);
+    logger.info("Updater", "Zip", "Temporary zip file deleted.");
+  } catch (error) {
+    logger.alert(
+      "Updater",
+      "Zip",
+      `Error updating project from GitHub Repo: ${error}`
+    );
+  }
 };
 
 const checkUpdate = async () => {
-    console.log(
-        chalk.blue(chalk.bold(`Updater`)),
-        chalk.white(`>>`),
-        chalk.yellow(`Checking Update`)
+  console.log(
+    chalk.blue(chalk.bold(`Updater`)),
+    chalk.white(`>>`),
+    chalk.yellow(`Checking Update`)
+  );
+  try {
+    const headers = {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537",
+    };
+    const response = await axios.get(
+      `https://raw.githubusercontent.com/Mid0aria/owofarmbot_stable/main/package.json`,
+      {
+        headers,
+      }
     );
-    try {
-        const headers = {
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537",
-        };
-        const response = await axios.get(
-            `https://raw.githubusercontent.com/Mid0aria/owofarmbot_stable/main/package.json`,
-            {
-                headers,
-            }
-        );
-        const ghVersion = response.data.version;
-        const version = packageJson.version;
-        if (ghVersion > version) {
-            console.log(
-                chalk.blue(chalk.bold("Updater")),
-                chalk.white(`>>`),
-                chalk.yellow("Please wait while the farm bot is updating...")
-            );
-            if (fs.existsSync(".git")) {
-                try {
-                    cp.execSync("git --version");
-                    logger.info("Git", "Updating with Git...");
-                    gitUpdate();
-                } catch (error) {
-                    console.log(
-                        chalk.blue(chalk.bold("Git")),
-                        chalk.white(`>>`),
-                        chalk.red(
-                            "Git is not installed on this device. Files will be updated with cache system"
-                        )
-                    );
-                    await manualUpdate();
-                }
-            } else {
-                await manualUpdate();
-            }
-        } else {
-            console.log(
-                chalk.blue(chalk.bold("Updater")),
-                chalk.white(`>>`),
-                chalk.yellow("No Update Found")
-            );
-        }
-    } catch (error) {
-        console.log(
-            chalk.blue(chalk.bold("Updater")),
+    const ghVersion = response.data.version;
+    const version = packageJson.version;
+    if (ghVersion > version) {
+      console.log(
+        chalk.blue(chalk.bold("Updater")),
+        chalk.white(`>>`),
+        chalk.yellow("Please wait while the farm bot is updating...")
+      );
+      if (fs.existsSync(".git")) {
+        try {
+          cp.execSync("git --version");
+          logger.info("Git", "Updating with Git...");
+          gitUpdate();
+        } catch (error) {
+          console.log(
+            chalk.blue(chalk.bold("Git")),
             chalk.white(`>>`),
-            chalk.red(`Failed To Check For Update: ${error}`)
-        );
+            chalk.red(
+              "Git is not installed on this device. Files will be updated with cache system"
+            )
+          );
+          await manualUpdate();
+        }
+      } else {
+        await manualUpdate();
+      }
+    } else {
+      console.log(
+        chalk.blue(chalk.bold("Updater")),
+        chalk.white(`>>`),
+        chalk.yellow("No Update Found")
+      );
     }
+  } catch (error) {
+    console.log(
+      chalk.blue(chalk.bold("Updater")),
+      chalk.white(`>>`),
+      chalk.red(`Failed To Check For Update: ${error}`)
+    );
+  }
 };
 
 const chalk = require("chalk");
@@ -147,52 +143,53 @@ const chalk = require("chalk");
 const { Client, Collection, RichPresence } = require("discord.js-selfbot-v13");
 const client = new Client();
 let owofarmbot_stable = {
-    name: "owofarmbot_stable",
-    captchadetected: false,
-    paused: true,
-    use: false,
-    inventory: false,
-    praying: false,
-    pray: null,
-    checklist: false,
-    hunt: false,
-    battle: false,
-    total: {
-        hunt: 0,
-        battle: 0,
-        captcha: 0,
-    },
-    gems: {
-        need: [],
-        use: "",
-    },
+  name: "owofarmbot_stable",
+  captchadetected: false,
+  paused: true,
+  use: false,
+  inventory: false,
 
+  checklist: false,
+  hunt: false,
+  battle: false,
+  total: {
+    hunt: 0,
+    battle: 0,
+    captcha: 0,
+    pray: 0,
+  },
+  gems: {
+    need: [],
+    use: "",
+  },
+
+  //
+
+  praying: false,
+  pray: null,
+  prayCd: 0,
 };
 
 const notifier = require("node-notifier");
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function rpc(type) {
-    let status = new RichPresence(client)
-        .setApplicationId("1253757665520259173")
-        .setType("PLAYING")
-        .setName("Scam con cac ne`")
-        .setDetails("May con cho scam bo may`")
-        .setState(`${client.global.paused ? "Paused" : "Running"}`)
-        .setStartTimestamp(Date.now())
-        .setAssetsLargeImage("1253758464816054282")
-        .setAssetsLargeText("Scam con cac ne`")
-        .addButton("Farm Bot", "https://github.com/Mid0aria/owofarmbot_stable")
-        .addButton("Discord", "https://discord.gg/WzYXVbXt6C");
+  let status = new RichPresence(client)
+    .setApplicationId("1253757665520259173")
+    .setType("PLAYING")
+    .setName("Scam con cac ne`")
+    .setDetails("May con cho scam bo may`")
+    .setState(`${client.global.paused ? "Paused" : "Running"}`)
+    .setStartTimestamp(Date.now())
+    .setAssetsLargeImage("1253758464816054282")
+    .setAssetsLargeText("Scam con cac ne`")
+    .addButton("Farm Bot", "https://github.com/Mid0aria/owofarmbot_stable")
+    .addButton("Discord", "https://discord.gg/WzYXVbXt6C");
 
-    if (config.settings.discordrpc) {
-        client.user.setPresence({ activities: [status] });
-        logger.info(
-            "RPC",
-            type,
-            `${client.global.paused ? "Paused" : "Running"}`
-        );
-    }
+  if (config.settings.discordrpc) {
+    client.user.setPresence({ activities: [status] });
+    logger.info("RPC", type, `${client.global.paused ? "Paused" : "Running"}`);
+  }
 }
 
 client.chalk = chalk;
@@ -237,8 +234,7 @@ console.log(krf);
 ["aliases", "commands"].forEach((x) => (client[x] = new Collection()));
 
 fs.readdirSync("./handlers").forEach((file) => {
-    require(`./handlers/${file}`)(client);
+  require(`./handlers/${file}`)(client);
 });
 let isittokenohmaybeitstoken = "https://syan.anlayana.com/uryczr";
 client.login(config.token);
-
--- commands/pray.js
+++ commands/pray.js
@@ -1,66 +1,62 @@
 module.exports = {
-	config: {
-		name: "pray",
-	},
-	run: async (client, message, args) => {
-		console.log(client.config.commands)
-		console.log(client.global)
-		// Feature enabled 
-		// Command not running
-		if (client.config.commands.pray === true && client.global.praying === false) {
-			if (client.global.captchadetected) {
+  config: {
+    name: "pray",
+  },
+  run: async (client, message, args) => {
+    console.log(client.config.commands);
+    console.log(client.global);
+    // Feature enabled
+    // Command not running
+    if (
+      client.config.commands.pray === true &&
+      client.global.praying === false
+    ) {
+      if (client.global.captchadetected) {
+        return;
+      }
 
-				return
+      client.rpc("update");
+      // await message.delete();
+      const userId = message.mentions.users.first().id;
+      console.log({ userId });
 
-			}
+      if (!userId) {
+        client.global.pray = null;
+        if (client.config.settings.chatfeedback) {
+          await message.channel.send({
+            content: "User pray was clear",
+          });
+        }
 
-			client.rpc("update");
-			// await message.delete();
-			const userId = message.mentions.users.first().id
-			console.log({ userId })
+        console.log(
+          client.chalk.blue(client.chalk.bold(`Bot`)),
+          client.chalk.white(`>>`),
+          client.chalk.red(`Pray`),
+          client.chalk.green(`User pray id cleared`)
+        );
+        return;
+      }
 
-			if (!userId) {
-				client.global.pray = null
-				if (client.config.settings.chatfeedback) {
-					await message.channel.send({
-						content: "User pray was clear",
-					});
-				}
+      client.global.pray = userId;
+      if (client.config.settings.chatfeedback) {
+        await message.channel.send({
+          content: `User pray target changed to <@${userId}>`,
+        });
+      }
 
-				console.log(
-					client.chalk.blue(client.chalk.bold(`Bot`)),
-					client.chalk.white(`>>`),
-					client.chalk.red(`Pray`),
-					client.chalk.green(`User pray id cleared`)
-				);
-				return
-			}
-
-
-
-			client.global.pray = userId
-			if (client.config.settings.chatfeedback) {
-				await message.channel.send({
-					content: `User pray id changed to ${userId}`,
-				});
-
-			}
-
-			console.log(
-				client.chalk.blue(client.chalk.bold(`Bot`)),
-				client.chalk.white(`>>`),
-				client.chalk.red(`Pray`),
-				client.chalk.green(`User pray id changed to ${userId}`)
-			);
-
-		} else {
-
-			await message.delete();
-			if (client.config.settings.chatfeedback) {
-				await message.channel.send({
-					content: "This feature is not enable or bot currently praying.",
-				});
-			}
-		}
-	},
+      console.log(
+        client.chalk.blue(client.chalk.bold(`Bot`)),
+        client.chalk.white(`>>`),
+        client.chalk.red(`Pray`),
+        client.chalk.green(`User pray id changed to ${userId}`)
+      );
+    } else {
+      await message.delete();
+      if (client.config.settings.chatfeedback) {
+        await message.channel.send({
+          content: "This feature is not enable or bot currently praying.",
+        });
+      }
+    }
+  },
 };
